cd58c0417187a4e18e38a9c48cf6c98a5be498e9
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-10 22:19:58 -0300

    FeaturedMovie style

--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -1,3 +1,5 @@
+import { BiPlay, BiPlus } from "react-icons/bi";
+
 import "./style.css";
 
 const apiUrlImg = import.meta.env.VITE_API_IMG_ORIGINAL;
@@ -19,7 +21,7 @@ const FeaturedMovie = ({ item }) => {
         <div className="featured-horizontal">
           <div className="featured-name">{item.name}</div>
           <div className="featured-info">
-            <div className="featured-points">{item.vote_average} pontos</div>
+            <div className="featured-points">{item.vote_average.toFixed(1)} pontos</div>
             <div className="featured-year">{firstDate.getFullYear()}</div>
             <div className="featured-seasons">
               {item.number_of_seasons} temporada
@@ -27,8 +29,15 @@ const FeaturedMovie = ({ item }) => {
             </div>
             <div className="featured-description">{item.overview}</div>
             <div className="featured-buttons">
-              <a href="">Assistir</a>
-              <a href="">Minha Lista</a>
+              <a href={`/watch/${item.id}`} className="featured-watch-button">
+                <BiPlay className="watch-button" /> Assistir
+              </a>
+              <a
+                href={`/list/add/${item.id}`}
+                className="featured-mylist-button"
+              >
+                <BiPlus className="mylist-button" /> Minha Lista
+              </a>
             </div>
             <div className="featured-genres">
               <strong>Gêneros:</strong> {genres.join(", ")}
