68285b2e506ad8873b21b9299fc7da1822cd4c2a
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-10 21:46:49 -0300

    add react-icons

--- src/components/FeaturedMovie/index.jsx
+++ src/components/FeaturedMovie/index.jsx
@@ -3,7 +3,13 @@ import "./style.css";
 const apiUrlImg = import.meta.env.VITE_API_IMG_ORIGINAL;
 
 const FeaturedMovie = ({ item }) => {
-  console.log(item);
+  let firstDate = new Date(item.first_air_date);
+  let genres = [];
+
+  for (let i in item.genres) {
+    genres.push(item.genres[i].name);
+  }
+
   return (
     <section
       className="featured"
@@ -14,20 +20,18 @@ const FeaturedMovie = ({ item }) => {
           <div className="featured-name">{item.name}</div>
           <div className="featured-info">
             <div className="featured-points">{item.vote_average} pontos</div>
-            <div className="featured-year">2099</div>
+            <div className="featured-year">{firstDate.getFullYear()}</div>
             <div className="featured-seasons">
               {item.number_of_seasons} temporada
               {item.number_of_seasons !== 1 ? "s" : ""}
             </div>
             <div className="featured-description">{item.overview}</div>
             <div className="featured-buttons">
-
+              <a href="">Assistir</a>
+              <a href="">Minha Lista</a>
             </div>
             <div className="featured-genres">
-              <strong>Gêneros:</strong>
-              {item.genres.map((item) => {
-                console.log(item)
-              })}
+              <strong>Gêneros:</strong> {genres.join(", ")}
             </div>
           </div>
         </div>
