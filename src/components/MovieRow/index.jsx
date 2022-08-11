312219d009b1328e5fa9813d6715051b961b5f78
Lucas Morais <lucas.msantos68@senacsp.edu.br>
2022-08-11 17:17:04 -0300

    add MovieRow features

--- src/components/MovieRow/index.jsx
+++ src/components/MovieRow/index.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import {
   MdOutlineArrowBackIos,
   MdOutlineArrowForwardIos,
@@ -8,19 +9,43 @@ import "./style.css";
 const apiUrlImg = import.meta.env.VITE_API_IMG;
 
 const MovieRow = ({ title, items }) => {
+  const [scrollX, setScrollX] = useState(-400);
+
+  const handleLeftArrow = () => {
+    let valueOfX = scrollX + Math.round(window.innerWidth / 2);
+
+    if (valueOfX > 0) valueOfX = 0;
+
+    setScrollX(valueOfX);
+  };
+
+  const handleRightArrow = () => {
+    let valueOfX = scrollX - Math.round(window.innerWidth / 2);
+    let listWidth = items.results.length * 150;
+
+    if ((window.innerWidth - listWidth) > valueOfX) {
+      valueOfX = (window.innerWidth - listWidth) - 60;
+    }
+    
+    setScrollX(valueOfX);
+  };
+
   return (
     <div className="movieRow">
       <h2>{title}</h2>
 
-      <div className="movieRow-left">
-        <MdOutlineArrowBackIos className="movieRow-left-icon"/>
+      <div className="movieRow-left" onClick={handleLeftArrow}>
+        <MdOutlineArrowBackIos className="movieRow-left-icon" />
       </div>
-      <div className="movieRow-right">
-        <MdOutlineArrowForwardIos className="movieRow-right-icon"/>
+      <div className="movieRow-right" onClick={handleRightArrow}>
+        <MdOutlineArrowForwardIos className="movieRow-right-icon" />
       </div>
 
       <div className="movieRow-listArea">
-        <div className="movieRow-list">
+        <div
+          className="movieRow-list"
+          style={{ marginLeft: scrollX, width: items.results.length * 150 }}
+        >
           {items.results.length > 0 &&
             items.results.map((item, key) => (
               <div key={key} className="movieRow-item">
@@ -33,7 +58,6 @@ const MovieRow = ({ title, items }) => {
             ))}
         </div>
       </div>
-      
     </div>
   );
 };
